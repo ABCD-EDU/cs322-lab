94d87f60b5e49a090f82c3ff1a460c332669edaa
arevalolance <lancearevalo2000@gmail.com>
2022-03-04 14:59:17 +0800

    add: splitter for test and training dataset

---
+++ src/tools/splitter.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import os
+
+outDir = '../../res'
+
+def run():
+    training    = input("Input the percentage of training data: ")
+    filePath    = input("Input filename of the file to be split (ex. fileName.csv): ")
+
+    print("Splitting data with ", training, "/", 100-int(training), "split")
+
+    train_percentage = int(training) / 100
+
+    data = pd.read_csv("../../res/" + filePath)
+    training_split = data.sample(frac = train_percentage)
+    test_split = data.drop(training_split.index)
+
+    print("Training size: ", len(training_split))
+    print("Test size: ", len(test_split))
+
+    outName = filePath.replace(".csv", "")
+    fullPath = os.path.join(outDir, outName)    
+
+    if not os.path.exists(fullPath):
+        os.mkdir(fullPath)
+
+    training_split.to_csv(fullPath + "/training.csv", encoding="utf8")
+    test_split.to_csv(fullPath + "/test.csv", encoding="utf8")
+
+if __name__ == "__main__":
+    run()
